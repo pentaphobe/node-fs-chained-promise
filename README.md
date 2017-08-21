228a341c981087915b4b04ba5176ee58b2cba048
keilin.olsen <keilin.olsen@heathwallace.com>
2017-08-21 16:17:48 +1000

    add tests, some WIP functionality

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ important functionality is punctuated by constant error checks, try-catch blocks
 
 The purpose of this library is to explore the possibilities of simplifying this to (eg.):
 
+
 ```js
 
 // a common task in my tools
@@ -19,15 +20,14 @@ fsChain()
     .fail( (chain) => chain.mkdir(newPath) )
   .readFile()
     .fail( (chain) => {
-      chain.writeFileSync(filename, 
-    }
+      chain.writeFileSync(filename, 'blah')
+    })
   .then( (data) => {
     console.log(`file contents: '${data}'`);
   })
   .catch( (reason) => {
     console.log(`Had some issues here: '${reason}'`);
   })
-
 ```
 
 ## Thoughts
@@ -41,3 +41,76 @@ fsChain()
   - [ ] might be nicer to have a few variants for commonly used errors, eg.
     - [ ] `failFunc(funcName, handler)` - calls handler if failure originated with function `funcName`
     - [ ] `failEx(exceptionType, handler)` - calls handler if an exception of `exceptionType` was thrown
+
+## Alternative syntax ideas
+
+How to make promises look like callback hell (urgh)
+```js
+fs()
+  .chdir('something')
+    .catch( (err, fs) => {
+      fs
+        .mkdir('something')
+        .catch( (err, fs) => {
+          console.error(`permission issue or something`);
+        })
+    })
+
+
+
+```
+
+Onto something, but it may not be workable/..
+```js
+fs()
+  .chdir('something')
+    .catch()
+    .mkdir('something')
+      // implicit catch() fallback failure
+    .retry() // go back up a level and try again
+  .chdir('another')
+    .catch()
+    .mkdir('another')    
+      // implicit catch() fallback failure
+    .retry() // go back up a level and try again
+  .readFile('config.txt')
+    .catch()
+    .writeFile('config.txt', `key=value`)
+      .catch( (err, fs) => {
+        // actual catch method can be skipped unless custom output is needed
+      })
+    .retry()
+
+
+// Happy path:
+chdir('something') -> readFile('config.txt')
+
+// Worst case scenario
+chdir('something') -> mkdir('something') -> chdir('something') -> readFile('config.txt') -> writeFile('config.txt') -> readFile('config.txt')
+
+```
+
+A less contrived example
+```js
+
+const dataPath = 'here';
+const filename = 'foobar.txt';
+const joined = path.join(dataPath, filename);
+
+const defaultContent = 'hello there';
+
+fs()
+  .readFile(joined)
+    // catch() can be called empty, in which case it initiates a sub-branch chain which is only terminated by fatal(), retry(), resume(), or done()
+    .catch()
+    .mkdir(dataPath)
+      // fatal() is same as catch() but terminates the chain no matter what
+      .fatal( (err) => console.error(`couldn't make dir ${dataPath}`, err) )
+    .writeFile(joined, defaultContent)
+      .fatal( (err) => console.error(`couldn't make file ${joined}`, err) )
+    .retry() // performs the readFile() again
+  .
+  .done( () => {
+    console.log('complete');
+  })
+```
--- test/index.js
+++ test/index.js
@@ -1,18 +1,106 @@
 import * as library from '../lib/index';
+// import * as library from '../lib/alt';
 import { expect } from 'chai';
 
-console.dir(library);
+// console.dir(library);
+
+// describe('Library', () => {
+//   describe('Fake test default export', () => {
+//     it('should be a function', () => {
+//       let fn = library.default;
+//       expect(fn).to.be.a('function');
+//     });
+
+//     it('should multiply values by 2', () => {
+//       let result = library.default(4);
+//       expect(result).to.equal(8);
+//     });
+//   });
+// });
+// 
+
+let fs = library.default;
 
 describe('Library', () => {
-  describe('Fake test default export', () => {
-    it('should be a function', () => {
-      let fn = library.default;
-      expect(fn).to.be.a('function');
-    });
+  describe('error handling', () => {
+    it('should catch errors', (done) => {
+      fs()
+        ._run( 'forceTypeError()', () => this.nonexistant )
+        .then( () => {
+          // console.log('no error');
+          done('should have caught an error');
+        })
+        .catch( (err) => {
+          // console.log('error', err.error.name, err.source.name);
+          done();
+        })
+        .go()
+    })
 
-    it('should multiply values by 2', () => {
-      let result = library.default(4);
-      expect(result).to.equal(8);
-    });
-  });
+    it('should resolve on no error', (done) => {
+      fs()
+        ._run( 'do nothing', () => {} )
+        .then( () => {
+          // console.log('no error');
+          done();
+        })
+        .catch( (err) => {
+          done(`got error ${err}`);
+        })
+        .go()
+    })
+  })
+
+  describe('chains', () => {
+    it('should chain methods', (done) => {
+      fs()
+        ._run( 'do nothing', () => 'hi' )
+        ._run( 'something', () => {let f = 'figgles'; return f;} )
+        ._run( 'return a value', () => 23 * 666 )
+        .then( (val) => {
+          //console.log('success!  values:', val);
+          done();
+        })
+        .catch( (err) => {
+          done(`got error ${err}`)
+        })
+        .go()
+    })
+
+    it('multiple results should become an array', (done) => {
+      const values = ['hi', 'figgles', 23 * 666];
+
+      fs()
+        ._run( 'do nothing', () => values[0] )
+        ._run( 'log something', () => values[1] )
+        ._run( 'return a value', () => values[2] )
+        .then( (val) => {
+          expect(val).deep.equals(values);
+          done();
+        })
+        .catch( (err) => {
+          done(`got error ${err}`)
+        })
+        .go()
+    }) 
+
+    it('failures should never hit then()', (done) => {
+      const values = ['hi', 'figgles', 23 * 666];
+
+      fs()
+        ._run( 'do nothing', () => values[0] )
+        ._run( 'log something', () => global.someUnlikelyValue.Not.Really )
+        ._run( 'return a value', () => values[2] )
+        .then( (val) => {
+          done('failure is success');
+        })
+        .catch( (err, vals) => {
+          done();
+        })
+        .go()
+    }) 
+
+
+  }); 
+
 });
